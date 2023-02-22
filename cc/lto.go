ff9597bd4aa3a2e74a9f9dc3078d4b4f580f9a6e
piscesmk2013 <piscesmk2013@gmail.com>
2023-02-23 07:47:02 +0800

    Revert "lto: force full in place of thin"
    This reverts commit 5848012cd9995f7f03987e94e531079d957ca4af.

--- cc/lto.go
+++ cc/lto.go
@@ -91,9 +91,14 @@ func (lto *lto) flags(ctx BaseModuleContext, flags Flags) Flags {
 	if lto.LTO(ctx) {
 		var ltoCFlag string
 		var ltoLdFlag string
-		// HACK: use full lto opposed to thin lto.
-		// We do not care about compile times for runtime improvement.
-		ltoCFlag = "-flto"
+		if lto.ThinLTO() {
+			ltoCFlag = "-flto=thin -fsplit-lto-unit"
+		} else if lto.FullLTO() {
+			ltoCFlag = "-flto"
+		} else {
+			ltoCFlag = "-flto=thin -fsplit-lto-unit"
+			ltoLdFlag = "-Wl,--lto-O0"
+		}
 
 		flags.Local.CFlags = append(flags.Local.CFlags, ltoCFlag)
 		flags.Local.LdFlags = append(flags.Local.LdFlags, ltoCFlag)
