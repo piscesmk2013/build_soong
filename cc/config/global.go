5500ce9f47a2d7ad98486f1274343e96156af204
John Galt <johngaltfirstrun@gmail.com>
2023-07-19 10:08:47 -0400

    cc/config: bump clang to 17.0.4

--- cc/config/global.go
+++ cc/config/global.go
@@ -245,6 +245,13 @@ var (
 		"-Wno-error=strict-prototypes",
 		"-Wno-error=enum-conversion",
 		"-Wno-error=unused-value",
+		"-Wno-error=single-bit-bitfield-constant-conversion",
+		"-Wno-error=unused-private-field",
+		"-Wno-unused-command-line-argument",
+		"-Wno-error=thread-safety-analysis",
+		"-Wno-error=unguarded-availability",
+		"-Wno-error=logical-op-parentheses",
+		"-Wno-error=shadow-uncaptured-local",
 	}
 
 	noOverride64GlobalCflags = []string{}
@@ -310,7 +317,7 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r487747c"
+	ClangDefaultVersion      = "clang-r498229b"
 	ClangDefaultShortVersion = "17"
 
 	// Directories with warnings from Android.bp files.
