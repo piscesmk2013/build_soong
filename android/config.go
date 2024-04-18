075ed1fd6a71949e88e75a54b58fb7de2607d111
piscesmk2013 <piscesmk2013@gmail.com>
2024-04-18 21:48:15 +0800

    Revert "build/soong: make mimalloc opt in [3/3]"
    This reverts commit 53961777b7876cabb9057424eeb0b74dfba2dda5.

--- android/config.go
+++ android/config.go
@@ -1103,10 +1103,6 @@ func (c *config) DisableScudo() bool {
 	return !Bool(c.productVariables.Malloc_use_scudo)
 }
 
-func (c *config) DisableMimalloc() bool {
-	return !Bool(c.productVariables.Malloc_use_mimalloc)
-}
-
 func (c *config) Android64() bool {
 	for _, t := range c.Targets[Android] {
 		if t.Arch.ArchType.Multilib == "lib64" {
